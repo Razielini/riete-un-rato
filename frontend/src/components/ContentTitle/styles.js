ad2f2b24396a8191da8f7493e6f921bddfec274b
Razielini <razielco@gmail.com>
2020-06-10 17:27:23 -0600

    Create styles.js

---
+++ frontend/src/components/ContentTitle/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Title = styled.div`
+  width: 100%;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+  border-bottom: 1px solid rgb(56, 68, 77);
+  position: sticky;
+  span {
+    font-size: 25px;
+    font-weight: 500;
+  }
+`
