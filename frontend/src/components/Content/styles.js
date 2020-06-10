bbb951ebccd7b143d87699e1f522ef335278ee61
Razielini <razielco@gmail.com>
2020-06-10 17:27:07 -0600

    Create styles.js

---
+++ frontend/src/components/Content/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Section = styled.section`
+  border-left: 1px solid rgb(56, 68, 77);
+  border-right: 1px solid rgb(56, 68, 77);
+  min-width: 300px;
+`
