d55b7a9b4f4e6b7c427cda851c7848a9cc719463
Razielini <razielco@gmail.com>
2020-06-10 17:27:10 -0600

    Create index.js

---
+++ frontend/src/components/ContentArticleBox/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { PostBox, Image, BoxInput, BoxTool, UserInfoName, UserInfoElements, BoxInfo } from './styles'
+import { ToolOption } from '../ToolOption'
+
+const IMG_DEFAULT = 'https://pbs.twimg.com/profile_images/378800000637224697/f1be358d9c197a0e563ab2b6594e39ae_bigger.png'
+export const ContentArticleBox = ({ avatar = IMG_DEFAULT}) => {
+  return (
+    <PostBox>
+      <Image src={avatar} />
+      <BoxInput>
+        <UserInfoName>Fulanito de Tal <UserInfoElements>@fulano - 22min</UserInfoElements></UserInfoName>
+        <BoxInfo>
+          The horrifying killings of George Floyd, Ahmaud Arbery, Breonna Taylor and far too many other Black people—and the protests they sparked—are shining a light on the brutal injustices that Black people experience every day…
+        </BoxInfo>
+        <BoxTool>
+          <ToolOption icon="dialog" />
+          <ToolOption icon="retweet" />
+          <ToolOption icon="heart" />
+        </BoxTool>
+      </BoxInput>
+    </PostBox>
+  )
+}
