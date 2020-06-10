5277193b7baa3229a43187078ec7098ef3828898
Razielini <razielco@gmail.com>
2020-06-10 17:27:15 -0600

    Create index.js

---
+++ frontend/src/components/ContentPostBox/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { PostBox, Image, Textarea, BoxInput, BoxTool } from './styles'
+
+const IMG_DEFAULT = 'https://pbs.twimg.com/profile_images/378800000637224697/f1be358d9c197a0e563ab2b6594e39ae_bigger.png'
+export const ContentPostBox = ({ avatar = IMG_DEFAULT }) => {
+  return (
+    <PostBox>
+      <Image src={avatar} />
+      <BoxInput>
+        <Textarea>¿Qué está pasando?</Textarea>
+        <BoxTool>
+          Tools
+        </BoxTool>
+      </BoxInput>
+    </PostBox>
+  )
+}
