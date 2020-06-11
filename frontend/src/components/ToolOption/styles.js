ab4628bd7852e5ad568caae1bc47f48135aa4662
Razielini <razielco@gmail.com>
2020-06-11 12:02:35 -0600

    Create styles.js

---
+++ frontend/src/components/ToolOption/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Li = styled.li`
+  font-size: 25px;
+  cursor: pointer;
+  padding: 10px 20px;
+  width: fit-content;
+  margin: 5px 0;
+  &:hover {
+    background-color: #794BC4;
+    border-radius: 30px;
+  }
+`
