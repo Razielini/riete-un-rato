71d79b7cdba652117671ebd6203e816e6c63f270
Razielini <razielco@gmail.com>
2020-06-11 12:02:31 -0600

    Create styles.js

---
+++ frontend/src/components/Sidebar/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Sidebar = styled.section`
+  width: 300px;
+  padding: 10px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+`
