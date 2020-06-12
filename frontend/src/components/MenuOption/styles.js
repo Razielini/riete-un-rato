47fcc0b9e1ee8b3b09f708e3b13dd65713ba4281
Razielini <razielco@gmail.com>
2020-06-12 14:22:43 -0600

    Update styles.js

--- frontend/src/components/MenuOption/styles.js
+++ frontend/src/components/MenuOption/styles.js
@@ -1,12 +1,31 @@
 import styled from 'styled-components'
+import { Link as LinkRouter} from '@reach/router'
 
 export const Li = styled.li`
-  font-size: 25px;
   cursor: pointer;
   padding: 10px 20px;
-  width: fit-content;
+  width: max-content;
   margin: 5px 0;
+  @media screen and (max-width: 1300px){
+    span {
+      display: none;
+    }
+  }
+`
+
+export const Link = styled(LinkRouter)`
+  font-size: 21px;
+  cursor: pointer;
+  padding: 10px 20px;
+  width: max-content;
+  margin: 5px 0;
+  text-decoration: none;
+  color: #FFF;
   &:hover {
+    background-color: #794BC4;
+    border-radius: 30px;
+  }
+  &[aria-current] {
     background-color: #794BC4;
     border-radius: 30px;
   }
