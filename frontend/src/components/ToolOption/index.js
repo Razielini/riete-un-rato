effb35c8fcccf90cdd24c3a36456c783cbb654f7
Razielini <razielco@gmail.com>
2020-06-12 14:23:04 -0600

    Update index.js

--- frontend/src/components/ToolOption/index.js
+++ frontend/src/components/ToolOption/index.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Dialog, Heart, Retweet } from '../Icons'
+import { Div } from './styles'
 
-export const ToolOption = ({ icon = 'heart' }) => {
-  const iconType = (icon) => {
-    if (icon === 'heart') return <Heart />
-    if (icon === 'dialog') return <Dialog />
-    if (icon === 'retweet') return <Retweet />
+export const ToolOption = ({ icon = 'heart', hoverColor }) => {
+  const [isHover, setIsHover] = useState(false);
+
+  const iconType = (icon, isHover, hoverColor) => {
+    if (icon === 'heart') return <Heart isHover={isHover} hoverColor={hoverColor} />
+    if (icon === 'dialog') return <Dialog isHover={isHover} hoverColor={hoverColor} />
+    if (icon === 'retweet') return <Retweet isHover={isHover} hoverColor={hoverColor} />
   }
 
   return (
-    <div>
+    <Div
+      hoverColor={hoverColor}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
       {
-        iconType(icon)
+        iconType(icon, isHover, hoverColor)
       }
       <span>123</span>
-    </div>
+    </Div>
   )
 }
