07d2accca268c78a70dc7cd9e328ec2291b0fdbd
Razielini <razielco@gmail.com>
2020-06-12 14:22:59 -0600

    Delete styles.js

--- frontend/src/components/Sidebar/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Sidebar = styled.section`
-  width: 300px;
-  padding: 10px;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-`
