26f36c789fb8ef3c3a3ca19cfc9104c53dec0f23
Razielini <razielco@gmail.com>
2020-06-13 17:36:05 -0600

    Update Default.js

--- frontend/src/layouts/Default.js
+++ frontend/src/layouts/Default.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { DefaultLayout } from './styles'
+import { Helmet } from 'react-helmet'
 
-export const Default = ({ children }) => {
+import { Header } from '../containers/Header'
+
+export const Default = ({ children, title, description }) => {
   return (
-    <>
+    <DefaultLayout>
+      <Helmet>
+        {title && <title>{title} | Riete un rato 🃏</title>}
+        {description && <meta name='description' content={description} />}
+      </Helmet>
+      <Header/>
       { children }
-    </>
+    </DefaultLayout>
   )
 }
