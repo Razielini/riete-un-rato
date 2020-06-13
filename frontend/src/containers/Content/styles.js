04b16fe5da641118fc9376df7d9c34205ef0ed52
Razielini <razielco@gmail.com>
2020-06-13 17:35:47 -0600

    Create styles.js

---
+++ frontend/src/containers/Content/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Section = styled.section`
+  border-left: 1px solid rgb(56, 68, 77);
+  border-right: 1px solid rgb(56, 68, 77);
+  max-width: 600px;
+  width: 100%;
+  display: grid;
+  grid-area: Content;
+`
+
+export const PostSection = styled.div`
+  margin-top: 5%;
+`
