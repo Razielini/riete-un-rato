0114efa97cb89523ea5322182b9013229e411631
Razielini <razielco@gmail.com>
2020-06-13 17:35:52 -0600

    Create styles.js

---
+++ frontend/src/containers/Header/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Header = styled.section`
+  width: auto;
+  box-sizing: border-box;
+  padding: 10px;
+  flex-shrink: 1;
+  display: flex;
+  justify-content: flex-end;
+  flex-grow: 1;
+  grid-area: Header;
+`
+
+export const Ul = styled.ul`
+  padding-inline-start: 0px;
+  list-style: none;
+  width: fit-content;
+  margin: 0;
+`
+
+export const Nav = styled.nav`
+  position: fixed;
+`
