5e9de94822732d34ea06736419a3ad41c97c34fb
Razielini <razielco@gmail.com>
2020-06-14 10:09:43 -0600

    Create GlobalStyles.js

---
+++ frontend/src/styles/GlobalStyles.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #15202B;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+
+  #root {
+    width: 100%;
+    box-sizing: border-box;
+    display: table;
+    justify-content: center;
+    background-color: #15202B;
+    color: #FFF;
+  }
+
+  @media screen and (max-width: 1024px) {
+    #root {
+      display: table;
+    }
+  }
+
+`
