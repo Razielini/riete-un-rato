2b8f1857987217193962a51122463a05afb84848
Razielini <razielco@gmail.com>
2020-06-14 10:09:36 -0600

    Create Profile.js

---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../logo.svg'
+import { Sidebar } from '../containers/Sidebar'
+import { Content } from '../containers/Content'
+import { Default } from '../layouts/Default'
+
+export const Profile = () => {
+  return (
+    <Default
+      className="App"
+      title="Perfil"
+      description="Super Description"
+    >
+      <Content>
+        <img src={logo} className="App-logo" alt="logo" />
+      </Content>
+      <Sidebar />
+    </Default>
+  )
+}
