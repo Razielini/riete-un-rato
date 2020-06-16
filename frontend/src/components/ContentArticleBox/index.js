f0fb19eb19586aa9f83a6f5ec99adcdf4e3c4bc8
Razielini <razielco@gmail.com>
2020-06-16 07:43:10 -0600

    Update index.js

--- frontend/src/components/ContentArticleBox/index.js
+++ frontend/src/components/ContentArticleBox/index.js
@@ -3,7 +3,25 @@ import { PostBox, Image, BoxInput, BoxTool, UserInfoName, UserInfoElements, BoxI
 import { ToolOption } from '../ToolOption'
 
 const IMG_DEFAULT = 'https://pbs.twimg.com/profile_images/378800000637224697/f1be358d9c197a0e563ab2b6594e39ae_bigger.png'
-export const ContentArticleBox = ({ avatar = IMG_DEFAULT}) => {
+const ICONS_DEFAULT = [
+  {
+    icon: 'dialog',
+    hoverColor: '#1B6FA6',
+    info: '123'
+  },
+  {
+    icon: 'retweet',
+    hoverColor: '#178A52',
+    info: '123'
+  },
+  {
+    icon: 'heart',
+    hoverColor: 'rgba(224, 36, 94)',
+    info: '123'
+  }
+]
+
+export const ContentArticleBox = ({ avatar = IMG_DEFAULT, icons = ICONS_DEFAULT}) => {
   return (
     <PostBox>
       <Image src={avatar} />
@@ -13,9 +31,9 @@ export const ContentArticleBox = ({ avatar = IMG_DEFAULT}) => {
           The horrifying killings of George Floyd, Ahmaud Arbery, Breonna Taylor and far too many other Black people—and the protests they sparked—are shining a light on the brutal injustices that Black people experience every day…
         </BoxInfo>
         <BoxTool>
-          <ToolOption icon='dialog' hoverColor='#1B6FA6' />
-          <ToolOption icon='retweet' hoverColor='#178A52' />
-          <ToolOption icon='heart' hoverColor='rgba(224, 36, 94)' />
+          {
+            icons.map(myIcon => <ToolOption icon={myIcon.icon} hoverColor={myIcon.hoverColor} info={myIcon.info} /> )
+          }
         </BoxTool>
       </BoxInput>
     </PostBox>
