a0a7ec06b287ed03ad9e31f6d85d3cb11cc6c348
Razielini <razielco@gmail.com>
2020-06-16 07:43:20 -0600

    Update index.js

--- frontend/src/components/ContentTitle/index.js
+++ frontend/src/components/ContentTitle/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Title } from './styles'
+import { Title, TitleContainer, SpanTitle, SpanSubtitle, PreOptionContainer, PostOptionContainer } from './styles'
+import { ToolOption } from '../ToolOption'
 
-export const ContentTitle = ({ title = 'inicio', option = 'none'}) => {
+export const ContentTitle = ({ title = 'inicio', subtitle = null, preoption = null, postoption = null}) => {
+
   return (
-    <Title>
-      <span>{ title}</span>
-      <span>{option}</span>
-    </Title>
+    <TitleContainer>
+      { preoption && <PreOptionContainer><ToolOption icon={preoption.icon} hoverColor={preoption.hoverColor} fillColor={preoption.fillColor} /></PreOptionContainer> }
+      <Title>
+        <SpanTitle>{ title }</SpanTitle>
+        { subtitle && <SpanSubtitle>{ subtitle }</SpanSubtitle>}
+      </Title>
+      { postoption && <PostOptionContainer><ToolOption icon={postoption.icon} hoverColor={postoption.hoverColor} fillColor={postoption.fillColor} /></PostOptionContainer> }
+    </TitleContainer>
   )
 }
