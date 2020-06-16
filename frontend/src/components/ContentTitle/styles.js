348504434d4758ef7339e0c7aab74704bde8ed73
Razielini <razielco@gmail.com>
2020-06-16 07:43:18 -0600

    Update styles.js

--- frontend/src/components/ContentTitle/styles.js
+++ frontend/src/components/ContentTitle/styles.js
@@ -1,14 +1,47 @@
 import styled from 'styled-components'
 
-export const Title = styled.div`
+export const TitleContainer = styled.div`
   width: 100%;
-  padding: 15px;
   display: flex;
   justify-content: space-between;
   box-sizing: border-box;
   border-bottom: 1px solid rgb(56, 68, 77);
-  span {
-    font-size: 25px;
-    font-weight: 500;
+`
+
+export const Title = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 10px 15px;
+  justify-content: space-between;
+  box-sizing: border-box;
+  flex-direction: column;
+`
+
+export const SpanTitle = styled.span`
+  font-size: 25px;
+  font-weight: 500;
+`
+
+export const SpanSubtitle = styled.span`
+  font-size: 14px;
+  font-weight: 300;
+  color: rgb(136, 153, 166);
+`
+
+export const PreOptionContainer = styled.div`
+  padding: 10px 15px;
+  align-self: center;
+  justify-self: center;
+  svg {
+    width: 23px;
+  }
+`
+
+export const PostOptionContainer = styled.div`
+  padding: 10px 15px;
+  align-self: center;
+  justify-self: center;
+  svg {
+    width: 23px;
   }
 `
