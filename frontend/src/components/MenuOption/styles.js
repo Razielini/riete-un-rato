60571e4778c20e496731379247525ec55762ffaf
Razielini <razielco@gmail.com>
2020-06-16 07:44:01 -0600

    Update styles.js

--- frontend/src/components/MenuOption/styles.js
+++ frontend/src/components/MenuOption/styles.js
@@ -3,10 +3,11 @@ import { Link as LinkRouter} from '@reach/router'
 
 export const Li = styled.li`
   cursor: pointer;
-  padding: 10px 20px;
+  padding: 5px 20px;
   width: max-content;
-  margin: 5px 0;
+  margin: 0;
   @media screen and (max-width: 1300px){
+    padding: 10px 0px;
     span {
       display: none;
     }
@@ -21,12 +22,18 @@ export const Link = styled(LinkRouter)`
   margin: 5px 0;
   text-decoration: none;
   color: #FFF;
+  display: flex;
+  align-items: center;
+  font-weight: 500;
   &:hover {
-    background-color: #794BC4;
+    background-color: rgba(121, 75, 196, 0.3);
+    color: #794BC4;
     border-radius: 30px;
   }
   &[aria-current] {
-    background-color: #794BC4;
-    border-radius: 30px;
+    color: #794BC4;
+    svg {
+      fill: #794BC4;
+    }
   }
 `
