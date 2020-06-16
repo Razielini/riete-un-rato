c40d7ee048630c66622eeb662309458c2b6761e7
Razielini <razielco@gmail.com>
2020-06-16 07:43:59 -0600

    Update index.js

--- frontend/src/components/MenuOption/index.js
+++ frontend/src/components/MenuOption/index.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Li, Link } from './styles'
-import { Cat, Bell, Bookmark, User, Home } from '../Icons'
+import { Cat, Bell, Bookmark, User, Home, Logo, OldPen } from '../Icons'
 
-export const MenuOption = ({ title, icon = 'bell', to }) => {
-  const iconType = (icon) => {
-    if (icon === 'home') return <Home />
-    if (icon === 'cat') return <Cat />
-    if (icon === 'bell') return <Bell />
-    if (icon === 'bookmark') return <Bookmark />
-    if (icon === 'user') return <User />
+export const MenuOption = ({ title, icon = 'bell', to, hoverColor, fillColor }) => {
+  const [isHover, setIsHover] = useState(false);
+
+  const iconType = (icon, isHover, hoverColor, fillColor) => {
+    icon = icon.toLowerCase()
+    if (icon === 'home') return <Home isHover={isHover} hoverColor={hoverColor} fillColor={fillColor} />
+    if (icon === 'cat') return <Cat isHover={isHover} hoverColor={hoverColor} fillColor={fillColor} />
+    if (icon === 'bell') return <Bell isHover={isHover} hoverColor={hoverColor} fillColor={fillColor} />
+    if (icon === 'bookmark') return <Bookmark isHover={isHover} hoverColor={hoverColor} fillColor={fillColor} />
+    if (icon === 'user') return <User isHover={isHover} hoverColor={hoverColor} fillColor={fillColor} />
+    if (icon === 'logo') return <Logo isHover={isHover} hoverColor={hoverColor} fillColor={fillColor} />
+    if (icon === 'oldpen') return <OldPen isHover={isHover} hoverColor={hoverColor} fillColor={fillColor} />
   }
 
   return (
-    <Li>
+    <Li
+      hoverColor={hoverColor}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
       <Link to={to}>
         {
-          iconType(icon)
+          iconType(icon, isHover, hoverColor, fillColor)
         }
         <span>{title}</span>
       </Link>
