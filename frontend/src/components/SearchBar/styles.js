7f3874fbd82869aca496d75d2170168332b0f1a6
Razielini <razielco@gmail.com>
2020-06-17 13:03:09 -0600

    Update styles.js

--- frontend/src/components/SearchBar/styles.js
+++ frontend/src/components/SearchBar/styles.js
@@ -3,6 +3,12 @@ import styled from 'styled-components'
 export const SearchInput = styled.input`
   width: 100%;
   border-radius: 10px;
-  padding: 5px;
+  padding: 5px 15px;
   box-sizing: border-box;
+  background-color: rgb(37, 51, 65);
+  border: 0px solid transparent;
+  min-height: 40px;
+  :focus{
+    outline: 0px solid transparent;
+  }
 `
