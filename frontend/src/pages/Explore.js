ed4b8fe61cb406e242f6bc4a089e97de7abdd37d
Razielini <razielco@gmail.com>
2020-06-17 13:03:22 -0600

    Update Explore.js

--- frontend/src/pages/Explore.js
+++ frontend/src/pages/Explore.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import logo from '../logo.svg'
 import { Sidebar } from '../containers/Sidebar'
 import { Content } from '../containers/Content'
 import { Default } from '../layouts/Default'
+
+import { ContentTitle } from '../components/ContentTitle'
+import { ContentPostBox } from '../components/ContentPostBox'
+import { ContentArticleBox} from '../components/ContentArticleBox'
 
 export const Explore = () => {
   return (
@@ -12,7 +15,21 @@ export const Explore = () => {
       description="Super Description"
     >
       <Content>
-        <img src={logo} className="App-logo" alt="logo" />
+        <ContentTitle title='Explorar' />
+        <ContentPostBox />
+        <div className="mt-5-p">
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+        </div>
       </Content>
       <Sidebar />
     </Default>
