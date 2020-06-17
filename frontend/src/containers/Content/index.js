68146caebb94d7a63b90260627d5e06e5e1db580
Razielini <razielco@gmail.com>
2020-06-17 13:03:14 -0600

    Update index.js

--- frontend/src/containers/Content/index.js
+++ frontend/src/containers/Content/index.js
@@ -1,28 +1,10 @@
 import React from 'react'
-import { Section, PostSection } from './styles'
-
-import { ContentTitle } from '../../components/ContentTitle'
-import { ContentPostBox } from '../../components/ContentPostBox'
-import { ContentArticleBox} from '../../components/ContentArticleBox'
+import { Section } from './styles'
 
 export const Content = ({ children }) => {
   return (
     <Section>
-      <ContentTitle />
-      <ContentPostBox />
-      <PostSection>
-        <ContentArticleBox />
-        <ContentArticleBox />
-        <ContentArticleBox />
-        <ContentArticleBox />
-        <ContentArticleBox />
-        <ContentArticleBox />
-        <ContentArticleBox />
-        <ContentArticleBox />
-        <ContentArticleBox />
-        <ContentArticleBox />
-        <ContentArticleBox />
-      </PostSection>
+      { children }
     </Section>
   )
 }
