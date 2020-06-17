e82df2ae0f5b038dd1a334a25332b944e0954b2b
Razielini <razielco@gmail.com>
2020-06-17 13:03:26 -0600

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import logo from '../logo.svg'
 import { Sidebar } from '../containers/Sidebar'
 import { Content } from '../containers/Content'
 import { Default } from '../layouts/Default'
+
+import { ContentTitle } from '../components/ContentTitle'
+import { ContentPostBox } from '../components/ContentPostBox'
+import { ContentArticleBox} from '../components/ContentArticleBox'
+
+const POSTOPTION = {
+  icon: 'Constellation',
+  color: 'rgb(121, 75, 196)',
+  fillColor: 'rgb(121, 75, 196)'
+}
 
 export const Home = () => {
   return (
@@ -12,7 +21,21 @@ export const Home = () => {
       description="Super Description"
     >
       <Content>
-        <img src={logo} className="App-logo" alt="logo" />
+        <ContentTitle />
+        <ContentPostBox title='Inicio' postoption={POSTOPTION} />
+        <div className="mt-5-p">
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+        </div>
       </Content>
       <Sidebar />
     </Default>
