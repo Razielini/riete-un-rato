c5a830e0613384d974fbb8fa5471eeb1ad28c603
Razielini <razielco@gmail.com>
2020-06-17 13:03:36 -0600

    Update Profile.js

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import logo from '../logo.svg'
 import { Sidebar } from '../containers/Sidebar'
 import { Content } from '../containers/Content'
 import { Default } from '../layouts/Default'
+
+import { ContentTitle } from '../components/ContentTitle'
+import { ContentArticleBox} from '../components/ContentArticleBox'
+import { ProfileHeader } from '../components/ProfileHeader'
 
 export const Profile = () => {
   return (
     <Default
       className="App"
-      title="Perfil"
+      title="Profile"
       description="Super Description"
     >
       <Content>
-        <img src={logo} className="App-logo" alt="logo" />
+        <ContentTitle title='Perfil' />
+        <ProfileHeader />
+        <div className="mt-5-p">
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+        </div>
       </Content>
       <Sidebar />
     </Default>
