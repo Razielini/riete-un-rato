3da4cecc516b61cff21a88cebb5af069dca9f17c
Razielini <razielco@gmail.com>
2020-06-17 13:03:29 -0600

    Update Notifications.js

--- frontend/src/pages/Notifications.js
+++ frontend/src/pages/Notifications.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import logo from '../logo.svg'
 import { Sidebar } from '../containers/Sidebar'
 import { Content } from '../containers/Content'
 import { Default } from '../layouts/Default'
+
+import { ContentTitle } from '../components/ContentTitle'
+import { ContentPostBox } from '../components/ContentPostBox'
+import { ContentArticleBox} from '../components/ContentArticleBox'
 
 export const Notifications = () => {
   return (
     <Default
       className="App"
-      title="Notifications"
+      title="Notificaciones"
       description="Super Description"
     >
       <Content>
-        <img src={logo} className="App-logo" alt="logo" />
+        <ContentTitle title='Notificaciones' />
+        <ContentPostBox />
+        <div className="mt-5-p">
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+          <ContentArticleBox />
+        </div>
       </Content>
       <Sidebar />
     </Default>
